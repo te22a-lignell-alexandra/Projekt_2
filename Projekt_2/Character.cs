5ec520244fb5e3e33a9ea6733609daf916708343
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2024-02-19 09:32:09 +0100

    spelarens karaktär syns på skärmen, kan röra sig åt alla håll

--- Projekt_2/Character.cs
+++ Projekt_2/Character.cs
@@ -1,6 +1,38 @@
+using System.Numerics;
 using Raylib_cs;
 
 class Character
 {
-    public Texture2D texture = Raylib.LoadTexture("");
+    public Texture2D image;
+    public Rectangle rect;
+    public Vector2 movement = new Vector2(0,0);
+
+// Från projekt 1, först kopierat från genomgång vv
+    public static Vector2 Movement(out Vector2 movement, float speed)
+{
+    movement = Vector2.Zero;
+
+        if (Raylib.IsKeyDown(KeyboardKey.A))
+        {
+            movement.X = -1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.D))
+        {
+            movement.X = 1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.W))
+        {
+            movement.Y = -1;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.S))
+        {
+            movement.Y = 1;
+        }
+        if (movement.Length() > 0)
+        {
+            movement = Vector2.Normalize(movement) * speed;
+        }
+
+    return movement;
+}
 }
--- Projekt_2/Program.cs
+++ Projekt_2/Program.cs
@@ -29,19 +29,44 @@ Raylib.InitWindow(1100, 900, "screen");
 Raylib.SetTargetFPS(60);
 
 
-// Vad ska finnas i spelet?
-// Labyrint, man ska kunna gå mellan rum genom dörrar, när man gått genom en dörr ska det vara som om skärmen
-// flyttats så att man nu ser det nya rummet, dvs att man hamnar på motsatt sida, som om man kom in från dörren
-// Inventory? Lista som man ska kunna bläddra i på något sätt, plocka upp och använd -> olika knappar, engångsgrejer
-// Om man kan lägga in att det blir mörkt nån sekund medans man byter scen
-// Låsa upp nya rum genom att göra saker, instruktioner till lösning visas vid låsta dörrar
+/* Vad ska finnas i spelet?
+Labyrint, man ska kunna gå mellan rum genom dörrar, när man gått genom en dörr ska det vara som om skärmen
+flyttats så att man nu ser det nya rummet, dvs att man hamnar på motsatt sida, som om man kom in från dörren
+Inventory? Lista som man ska kunna bläddra i på något sätt, plocka upp och använd -> olika knappar, engångsgrejer
+Om man kan lägga in att det blir mörkt nån sekund medans man byter scen (hur gör man för att nåt ska hända i x sekunder?)
+Låsa upp nya rum genom att göra saker, instruktioner till lösning visas vid låsta dörrar */
 
+/*För metoder 
+Skapa static void Name() {} metod. Lägg in koden. Kolla efter fel och rätta de som finns.
+Behövs inte värdet som variablen har från början kan man "skapa den igen" i metoden och sedan retunera med
+orginalvariabel = Metod(); i main. Gå inte direkt till parametrar eller ref! försök med return. 
 
+Lägga i klasser, skriv public före --> public static void Metod()
+Glöm ej Klassnamn.Metod() vid anropning*/
+
+Character playerCharacter = new() {image = Raylib.LoadTexture("img/wizard.png"), rect = new(500, 400, 64, 64)};
 
 
 
 while (!Raylib.WindowShouldClose())
 {
+/* --------------------------------------------------------------------------------------------------
+== MAIN ==
+----------------------------------------------------------------------------------------------------*/
+// Player movement
+playerCharacter.movement = Character.Movement(out playerCharacter.movement, 5);
+playerCharacter.rect.X += playerCharacter.movement.X;
+playerCharacter.rect.Y += playerCharacter.movement.Y;
 
+
+/*----------------------------------------------------------------------------------------------------
+== DRAWING ==
+-----------------------------------------------------------------------------------------------------*/
+Raylib.BeginDrawing();
+
+Raylib.ClearBackground(Color.DarkGreen);
+Raylib.DrawTexture(playerCharacter.image, (int) playerCharacter.rect.X, (int) playerCharacter.rect.Y, Color.White);
+
+Raylib.EndDrawing();
 
 }
